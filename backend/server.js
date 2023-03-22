d1d498f39aa752a95161b5c57fdfbb7e6453b980
osvaldo <iosvaldo.app@gmail.com>
2023-03-22 18:25:14 -0400

    REST API for goals 🏆

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express"); // handle api
+const colors = require('colors');
 const dotenv = require("dotenv").config(); // handle api
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
-// const mongoose = require('mongoose'); //handle database
+
+connectDB();
+
 
 const app = express(); // creating an express application
 
