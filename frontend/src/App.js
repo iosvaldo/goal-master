e9e08f8eae61c088a66f9b3479cafd89b754c868
osvaldo <iosvaldo.app@gmail.com>
2023-03-25 23:31:13 -0400

    added frontend folder and redux ⚛️

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+
+// import { Counter } from './features/counter/Counter';
+
+
+function App() {
+  return (
+    <div className="">
+      <h1>my App</h1>
+  
+    </div>
+  );
+}
+
+export default App;
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Mern app
+
+
+### run the backend using npm run server
+### run the frontend using npm run client inside the front end directory
